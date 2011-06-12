c0317583b00459de7ce09b2359943f6fa949d231
Gabriel Monnerat <gabriel@tiolive.com>
2011-06-12 19:43:16 -0300

    refactor and add new test

--- test_hello.py
+++ test_hello.py
@@ -3,9 +3,20 @@ from hello import app
 
 class HelloTestCase(unittest.TestCase):
 
+  def setUp(self):
+    self.client = app.test_client()
+
   def testHelloWorld(self):
-    client = app.test_client()
-    assert client.get("/").data == "Hello World!"
+    data = self.client.get("/").data
+    self.assertEquals(data,
+                      "Hello World!",
+                      data)
+
+  def testHelloRandomName(self):
+    data = self.client.get("/").data
+    self.assertEquals(data,
+                      "Hello Gabriel!",
+                      data)
 
 if __name__ == "__main__":
   unittest.main()
