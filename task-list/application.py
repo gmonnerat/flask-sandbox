09ef5f263c5c5502de011870ee94e71b1a352d96
Gabriel Monnerat <gabriel@tiolive.com>
2011-06-19 20:54:48 -0300

    implemented CRUD basically

--- task-list/application.py
+++ task-list/application.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, g
 from flaskext.mongoobject import MongoObject
 import settings
+from json import dumps
 
 app = Flask(__name__)
 
@@ -21,3 +22,24 @@ def new_task():
   task = Task(name=name, description=description)
   task.save()
   return "Saved!"
+
+@app.route("/delete", methods=["GET"])
+def delete_task():
+  id = request.args["id"]
+  # XXX - Not scalable way to delete one task
+  for task in Task.query.find():
+    if task.id == id:
+      task.remove()
+      break
+  return "Deleted: %s" % id
+
+@app.route("/", methods=["GET"])
+def list_tasks():
+  selector = lambda task: (task.id, task.name, task.description)
+  return dumps(map(selector, Task.query.find()))
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+  selector = lambda task: (task.id, task.name, task.description)
+  name = request.form["name"]
+  return dumps(map(selector, Task.query.find({"name":name})))
--- task-list/test_application.py
+++ task-list/test_application.py
@@ -1,13 +1,43 @@
 import unittest
 from application import app
+from json import loads
 
 class TaskListTestCase(unittest.TestCase):
 
+  def setUp(self):
+    self.client = app.test_client()
+
+  def tearDown(self):
+    response = self.client.get("/")
+    data = loads(response.data)
+    for id, name, description in data:
+      response = self.client.get("/delete?id=%s" % id)
+
   def test_add_task(self):
-    client = app.test_client()
-    response = client.post("/add", data={"name":"One Task",
+    response = self.client.post("/add", data={"name":"One Task",
                                    "description":"Do Something"})
     self.assertEquals(response.status_code, 200, response.status_code)
+
+  def test_delete_task(self):
+    self.client.post("/add", data={"name":"fix bug",
+                                   "description":"Fix Bug"})
+    response = self.client.post("/search", data=dict(name="fix bug"))
+    data = loads(response.data)
+    for id, name, description in data:
+      response = self.client.get("/delete?id=%s" % id)
+      self.assertEquals(200, response.status_code)
+
+  def test_list_tasks(self):
+    response = self.client.get("/")
+    data = loads(response.data)
+    self.assertEquals(data, [], data)
+
+  def test_find_one_task(self):
+    self.client.post("/add", data={"name":"fix something",
+                                   "description":"Fix Something"})
+    response = self.client.post("/search", data=dict(name="fix something"))
+    data = loads(response.data)
+    self.assertEquals(1, len(data))
 
 
 if __name__ == "__main__":
